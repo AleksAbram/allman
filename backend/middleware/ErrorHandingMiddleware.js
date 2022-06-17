8972512650923ed0db7e487871cd0019082d535b
Anton Mushnin <anton.mushnin@gmail.com>
2022-06-17 10:39:31 +0300

    middleware eslinted

--- backend/middleware/ErrorHandingMiddleware.js
+++ backend/middleware/ErrorHandingMiddleware.js
@@ -1,8 +1,8 @@
 const ApiError = require('../error/ApiError');
 
-module.exports = function (err, req, res, next) { // (ошибка,запрос, ответ, функция next);
-  if(err instanceof ApiError){
-   return res.status(err.status).json({message: err.message})
+module.exports = function (err, req, res) { // (ошибка,запрос, ответ, функция next);
+  if (err instanceof ApiError) {
+    return res.status(err.status).json({ message: err.message });
   }
-  return res.status(500).json({message: 'Непредвиденная ошибка!'})
-}
+  return res.status(500).json({ message: 'Непредвиденная ошибка!' });
+};
