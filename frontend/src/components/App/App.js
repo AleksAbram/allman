2c1b30b471b9f82e8be1c46ef0bf07711a2d48d8
MrST-7 <strukhachev7@gmail.com>
2022-06-17 18:19:56 +0300

    burger_menu

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,15 +1,20 @@
-import Header from '../Header/Header';
+// import Header from '../Header/Header';
+import NavState from '../../context/navState';
+import MainMenu from '../MainMenu/MainMenu';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Auth from '../Authorization/Authorization';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// import Auth from '../Authorization/Authorization';
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/auth" element={<Auth/>} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Header />
+    //   <Routes>
+    //     {/* <Route path="/auth" element={<Auth/>} /> */}
+    //   </Routes>
+    // </BrowserRouter>
+    <NavState>
+      <MainMenu />
+    </NavState>
   );
 }
 
