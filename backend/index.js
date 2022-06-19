90c2cc55f868696be2abbf5064b37318c75c242d
Anton Mushnin <anton.mushnin@gmail.com>
2022-06-19 23:37:47 +0300

    cors on

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const cors = require('cors');
 const path = require('path');
 const sequelize = require('./db');
 const models = require('./models/models');
 const router = require('./routes/index.routes');
 const errorHandler = require('./middleware/ErrorHandingMiddleware');
-// const cors = require('cors');
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-// app.use(cors())
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
