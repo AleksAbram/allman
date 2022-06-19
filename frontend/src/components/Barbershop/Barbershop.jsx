ce55ca7aed2d49be589f5a24eb39129256d5309c
Aleks <abramyan.aleksandr1@gmail.com>
2022-06-19 19:48:34 +0300

    apibbh

---
+++ frontend/src/components/Barbershop/Barbershop.jsx
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+
+
+function Barbershop(props) {
+    const [advice, setAdvice] = useState("");
+    const token = '6y6nzmdgrmdwctjcmgpg';
+    useEffect(()=> {
+        const bbh = async () => {
+            try{
+            const getFetch = await fetch('https://yclients.com/dashboard/266801', {
+                method:'GET',
+                headers: {
+                    'Authorization': `Bearer 6y6nzmdgrmdwctjcmgpg`,
+                }
+
+            });
+            console.log(getFetch);
+            const json = await getFetch.json();
+            console.log(json);
+            setAdvice(json.slip.advice)
+            } catch(error) {
+                console.log("error", error);
+            }
+        }
+        bbh();
+    }, [])
+    return (
+        <div>
+            <div> {advice}</div>
+        </div>
+    );
+}
+
+export default Barbershop;
