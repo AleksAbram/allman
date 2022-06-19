b2537e3188242a6f7516dc9866d762640007cc51
Anton Mushnin <anton.mushnin@gmail.com>
2022-06-19 18:20:55 +0300

    combine

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -2,13 +2,17 @@ import Header from '../Header/Header';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Auth from '../Authorization/Authorization';
+import Home from '../Home/Home';
+import Footer from '../Footer/Footer';
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/auth" element={<Auth/>} />
+        <Route path="/" element={<Home/>} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
