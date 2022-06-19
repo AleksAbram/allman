73bbfeea03bac07eb57c39c12ff7615dab2a0863
Anton Mushnin <anton.mushnin@gmail.com>
2022-06-19 17:20:36 +0300

    Footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="contact">
+        <div className="contact-label">telegram & whatsapp</div>
+        <div className="contact-contact">+7 (967) 804 - 44 - 48</div>
+      </div>
+      <div className="contact">
+        <div className="contact-label">instagram</div>
+        <div className="contact-contact">@all.man.saratov</div>
+      </div>
+      <div className="contact">
+        <div className="contact-label">по общим вопросам</div>
+        <div className="contact-contact">all.man.saratov@mail.ru</div>
+      </div>
+      <div className="contact">
+        <div className="contact-label">по поводу заказов</div>
+        <div className="contact-contact">all.man.saratov@mail.ru</div>
+      </div>
+    </div>
+  );
+}
+export default Footer;
+
