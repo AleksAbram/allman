51757213de2c84258eec8bf11d9d8c04c9c5aaf7
Aleks <abramyan.aleksandr1@gmail.com>
2022-06-20 14:38:07 +0300

    test

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const sequelize = require('./db');
 const models = require('./models/models');
 const router = require('./routes/index.routes');
-const errorHandler = require('./middleware/ErrorHandingMiddleware');
+// const errorHandler = require('./middleware/ErrorHandingMiddleware');
 // const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
@@ -17,7 +17,7 @@ app.use(fileUpload({}));
 app.use('/api', router);
 
 // Обработка ошибок. Должен идти последним!
-app.use(errorHandler);
+// app.use(errorHandler);
 
 const start = async () => {
   console.log(process.env.DATABASE_URL);
