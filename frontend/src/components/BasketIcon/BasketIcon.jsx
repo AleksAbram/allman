05f7138899eb80817bd307a10e259067a8e742d3
MrST-7 <strukhachev7@gmail.com>
2022-06-23 11:28:20 +0300

    fix_basketIcon

--- frontend/src/components/BasketIcon/BasketIcon.jsx
+++ frontend/src/components/BasketIcon/BasketIcon.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import './BasketIcon.css'
-
+import { useNavigate } from 'react-router-dom'
 const BasketIcon = () => {
-  const basket  = useSelector((state) => state.basket.basket);
+  const basket = useSelector((state) => state.basket.basket);
+  const navigate = useNavigate()
+
+
   return (
     <div className="bagCont">
-    <img src="/img/BAG.png" alt="x" className="bag" />
-    <span>{basket.length}</span>
-  </div>
+      <img src="/img/BAG.png" alt="x" className="bag" onClick={(e) => navigate('./basket')} />
+      {basket.length > 0 ? <span>{basket.length}</span> : ''}
+    </div>
   );
 };
 
