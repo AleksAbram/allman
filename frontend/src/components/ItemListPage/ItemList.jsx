8c7e54467bbff5b9993a86581bd70a323ecf8e89
Anton Mushnin <anton.mushnin@gmail.com>
2022-06-24 10:48:15 +0300

    release 1

--- frontend/src/components/ItemListPage/ItemList.jsx
+++ frontend/src/components/ItemListPage/ItemList.jsx
@@ -2,15 +2,19 @@ import { useSelector } from 'react-redux';
 // import { useNavigate } from 'react-router-dom';
 import ItemCard from './ItemCard';
 import "./ItemList.css";
+import Path from './Path';
 
-function ItemList() {
+function ItemList({path}) {
   const items = useSelector((state) => state.item.list);
   // const types = useSelector((state) => state.item.types);
 
 
   return (
-    <div className="items-list">
-        {items.map((item) => <ItemCard item={item} key={item.id}/>)}
+    <div className="list-container">
+      <Path path={path}/>
+      <div className="items-list">
+          {items.map((item) => <ItemCard item={item} key={item.id}/>)}
+      </div>
     </div>
   );
 }
--- frontend/src/components/ItemListPage/ItemListPage.jsx
+++ frontend/src/components/ItemListPage/ItemListPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
@@ -7,15 +8,16 @@ import ItemList from './ItemList';
 function ItemListPage() {
   const dispatch = useDispatch();
   const { type } = useParams();
+  const [path, setPath] = useState([]);
   //console.log(id);
   // const items = useSelector((state) => state.item.list);
   const types = useSelector((state) => state.item.types);
   const itemError = useSelector((state) => state.item.error);
   
   const getTypeById = (id) => {
-    console.log(types, id);
-    console.log(types.find((type) => type.id === id));
-    return types[types.find((type) => type.id === id)];
+   // console.log(types, id);
+    return types.find((type) => type.id === id);
+    // return types[types.find((type1) => type1.id === id)];
   } 
 
   const getParents = (type) => {
@@ -23,18 +25,39 @@ function ItemListPage() {
     if (parentId) {
       return `${getParents(parentId)} > ${getTypeById(parentId).type_name}`
     }
-    
+    else{
+      return getTypeById(parentId).type_name;
+    }
   }
-  const getItemPath = () => {
-    return `${getParents(type)} > ${getTypeById(type).type_name}`
+
+  const getPath = (id) => {
+    const parentId = getTypeById(id).type_parent;
+    const name = getTypeById(id).type_name;
+    if (!parentId)
+      return [{id, name}];
+    return [...getPath(parentId), {id, name}];
+  } 
+
+
+  const getItemPath = (id) => {
+    return `${getParents(id)} > ${getTypeById(id).type_name}`;
+   // return `> ${getTypeById(id).type_name}`
+
   }
+
+  // const pathClick = (e) => {
+  //   console.log(e);
+  //   console.log(`http://localhost:4000/items/types/${e.target.id}`);
+  // }
 
   useEffect(() => {
     console.log(type);
     dispatch(action.itemsFetch(type));
     dispatch(action.typesFetch());
-    // console.log("types: ", types);
-    //console.log(getItemPath(type));
+    console.log("types: ", types);
+    setPath(getPath(Number(type)));
+
+
 
 
     return () => {
@@ -50,8 +73,12 @@ function ItemListPage() {
           ? <p>{itemError}</p>
           :
           <> 
-
-            <ItemList />
+            {/* <div className="path">
+              {path.map((seg) => 
+                <div id={seg.id} className="path-seg" key={seg.id} onClick={pathClick}>{seg.name}</div>
+              )} 
+            </div> */}
+            <ItemList path={path}/>
           </>
       }
     </>
